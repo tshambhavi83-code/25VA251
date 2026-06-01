2a887196bad87ee9dfc63a12d7ae3966d960f0da
Shambhavi Tripathi <tshambhavi83@gmail.com>
2026-06-01 15:46:34 +0530

    add labexperiments.html/exp07/

---
+++ java/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+    public static void main (String[] args) {
+        String name= "Alice";
+        int age = 30;
+        System.out.println("Hello World!");
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+
+
+}
